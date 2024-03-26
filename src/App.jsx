cfe97da6c54a9777fce04f3b3aa2a29c7d9f3846
Adam Jiber <adamjiber@MacBook-Pro-som-tillhor-Adam.local>
2024-03-26 15:03:41 +0100

    Skapat navbar och hem komponenter och gjort lite små ändringar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './styling/App.css'
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <Router>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auktioner" element={<div>Auktioner</div>} />
+        </Routes>
+      </div> 
+    </Router>
+  );
 }
 
-export default App
+export default App;
