8379c40c372cc79b13023793f3c42ce82530eb40
sarah <sarah.emmoth@proton.me>
2024-04-02 10:40:11 +0200

    ta bort styling import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Footer from './components/Footer';
-import './styling/Home.css'
 
 
 function App() {
