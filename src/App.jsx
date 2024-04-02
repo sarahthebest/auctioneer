113cdaab0784a4c77cf2ecfb665577dd4a1c7d3f
sarah <sarah.emmoth@proton.me>
2024-04-02 10:37:04 +0200

    Importerat styling till app och home. Tog bort margin från root

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import Footer from './components/Footer';
+import './styling/Home.css'
+
 
 function App() {
   return (
     <Router>
-      <div>
+      <>
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auktioner" element={<div>Auktioner</div>} />
         </Routes>
         <Footer/>
-      </div> 
+      </> 
     </Router>
   );
 }
