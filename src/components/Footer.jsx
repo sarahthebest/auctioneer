548f7ea47586890957f4e4c496a482335932a361
J L <jl-@live.se>
2024-04-03 23:34:22 +0200

    fixat datum och tid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Box height={150} sx={{ bgcolor: "lightGrey", px:0, marginLeft:0 }}>
       <Container maxWidth='false' sx={{ display: "flex", flexDirection: "row", gap:2, p:1 , justifyContent:'space-between'}}>
-        <Typography sx={{pt:2, fontSize:22}} variant="h2">&copy; Auctioneer {new Date().getFullYear()}</Typography>
+        <Typography sx={{pt:2, fontSize:20,}} variant="h2">&copy; Auctioneer {new Date().getFullYear()}</Typography>
         <Stack gap={3} direction={'row'} sx={{paddingRight:5, pt:2}}>
         <Link to="https://github.com/sarahthebest/auctioneer" target="_blank">Github</Link>
         <Link to="/auktioner">Auktioner</Link>
