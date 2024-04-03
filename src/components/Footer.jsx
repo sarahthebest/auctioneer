b6fba18bb3b557f2e6e7b65e43af4d9ef5ff9b4a
Sarah <sarah.emmoth@proton.me>
2024-04-03 11:55:56 +0200

    Ändrat footer height.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <Box height={150} sx={{ bgcolor: "lightGrey", px:0, marginLeft:0 }}>
+    <Box height={120} sx={{ bgcolor: "lightGrey", px:0, marginLeft:0 }}>
       <Container maxWidth='false' sx={{ display: "flex", flexDirection: "row", gap:2, p:1 , justifyContent:'space-between'}}>
         <Typography sx={{pt:2, fontSize:22}} variant="h2">&copy; Auctioneer {new Date().getFullYear()}</Typography>
         <Stack gap={3} direction={'row'} sx={{paddingRight:5, pt:2}}>
