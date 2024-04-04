a46c356f3ea3a61fe093682337c58fed7e72ad11
sarah <sarah.emmoth@proton.me>
2024-04-04 11:18:59 +0200

    tagit bort {EndDate} och omformatterat med prettier

--- src/components/AuctionCard.jsx
+++ src/components/AuctionCard.jsx
@@ -3,75 +3,74 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 const AuctionCard = ({
-  AuctionId,
-  AuctionTitle,
-  AuctionDesc,
-  AuctionBid,
-  EndDate,
+    AuctionId,
+    AuctionTitle,
+    AuctionDesc,
+    AuctionBid,
+    EndDate,
 }) => {
 
-  const date = new Date (EndDate)
+    const date = new Date(EndDate)
 
-  return (
-    <Box sx={{ width: 240, height: "fit-content", position: "relative" }}>
-      <Link
-        to="/AuctionPage"
-        state={{
-          AuctionId: AuctionId,
-          AuctionTitle: AuctionTitle,
-          AuctionDesc: AuctionDesc,
-          AuctionBid: AuctionBid,
-          EndDate: EndDate,
-        }}
-      >
-        <Paper elevation={1}>
-          <Card variant="outlined" sx={{ height: 250 }}>
-            <CardContent>
-              <Grid
-                container
-                direction="column"
-                justifyContent="space-between"
-                sx={{ height: "100%" }}
-              >
-                <Grid item>
-                  <Typography sx={{ fontSize: 16 }} gutterBottom>
-                    {AuctionTitle}
-                  </Typography>
-                  <Typography
-                    sx={{ my: 2, color: "green" }}
-                    variant="h5"
-                    component="div"
-                  >
-                    {`${AuctionBid} kr`}
-                  </Typography>
-                  <Typography color="text.secondary">{AuctionDesc}</Typography>
-                </Grid>
-                <Grid item>
-                  <Typography
-                    variant="caption"
-                    color="text.secondary"
-                    sx={{ position: "absolute", bottom: 0, pb: 2 }}
-                  >
-                  {
-                   
-                   (date.getFullYear()) +  "-" +
-                   (date.getMonth() + 1 < 10 ? "0" +(date.getMonth() + 1) : date.getMonth() + 1) +"-" + 
-                   (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "-" +
-                   (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" +
-                   (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" +
-                   (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())
-                    
-                    }
-                    {EndDate}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </Card>
-        </Paper>
-      </Link>
-    </Box>
-  );
+    return (
+        <Box sx={{ width: 240, height: "fit-content", position: "relative" }}>
+            <Link
+                to="/AuctionPage"
+                state={{
+                    AuctionId: AuctionId,
+                    AuctionTitle: AuctionTitle,
+                    AuctionDesc: AuctionDesc,
+                    AuctionBid: AuctionBid,
+                    EndDate: EndDate,
+                }}
+            >
+                <Paper elevation={1}>
+                    <Card variant="outlined" sx={{ height: 250 }}>
+                        <CardContent>
+                            <Grid
+                                container
+                                direction="column"
+                                justifyContent="space-between"
+                                sx={{ height: "100%" }}
+                            >
+                                <Grid item>
+                                    <Typography sx={{ fontSize: 16 }} gutterBottom>
+                                        {AuctionTitle}
+                                    </Typography>
+                                    <Typography
+                                        sx={{ my: 2, color: "green" }}
+                                        variant="h5"
+                                        component="div"
+                                    >
+                                        {`${AuctionBid} kr`}
+                                    </Typography>
+                                    <Typography color="text.secondary">{AuctionDesc}</Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography
+                                        variant="caption"
+                                        color="text.secondary"
+                                        sx={{ position: "absolute", bottom: 0, pb: 2 }}
+                                    >
+                                        {
+
+                                            (date.getFullYear()) + "-" +
+                                            (date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1) + "-" +
+                                            (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "-" +
+                                            (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" +
+                                            (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" +
+                                            (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())
+
+                                        }
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </CardContent>
+                    </Card>
+                </Paper>
+            </Link>
+        </Box>
+    );
 };
 
 export default AuctionCard;
