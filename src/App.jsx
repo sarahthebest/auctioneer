c425a93c118b6ff44ff3c8b9c5ee89b05f8a38da
J L <jl-@live.se>
2024-04-04 22:18:47 +0200

    gjort några små ändringar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Homepage from './components/Homepage';
 import Footer from './components/Footer';
 import Auctions from './components/Auctions';
+
 
 import CreateAuctionForm from './components/CreateAuctionForm'; // Importera din CreateAuctionForm-komponent
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Homepage />} />
           <Route path="/auktioner" element={<Auctions />} />
           {/* Använd CreateAuctionForm direkt med gruppkoden */}
           <Route path="/skapa-auktion" element={<CreateAuctionForm groupCode="p7u" />} />
--- src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Home() {
-  return <div className='bebas-neue-regular'>Välkommen till Min Auktionsplats!</div>;
-}
-
-export default Home;
