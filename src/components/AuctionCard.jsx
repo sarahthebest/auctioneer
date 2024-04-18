25f0d62ad82e420296cd4b5bba8b78f5404aa00d
J L <jl-@live.se>
2024-04-18 11:26:49 +0200

    Små ändringar på time

--- src/components/AuctionCard.jsx
+++ src/components/AuctionCard.jsx
@@ -53,14 +53,12 @@ const AuctionCard = ({
                     sx={{ position: "absolute", bottom: 0, pb: 2 }}
                   >
                   {
-                   
                    (date.getFullYear()) +  "-" +
                    (date.getMonth() + 1 < 10 ? "0" +(date.getMonth() + 1) : date.getMonth() + 1) +"-" + 
-                   (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + "-" +
+                   (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " " + " " +
                    (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" +
                    (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" +
                    (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())
-                    
                     }
                   </Typography>
                 </Grid>
--- src/components/AuctionPage.jsx
+++ src/components/AuctionPage.jsx
@@ -72,6 +72,8 @@ const AuctionPage = () => {
     }
   };
 
+  const date = new Date (EndDate)
+
   return (
     <Box
       sx={{
@@ -168,10 +170,17 @@ const AuctionPage = () => {
               </div>
             </Container>
             <Container sx={{ display: "flex", flexDirection: "column" }}>
-              <Typography>Avslutas om:</Typography>
-              <div className="bid-time">
-                <span className="bid-countdown">{EndDate}</span>
-              </div>
+              <Typography>Avslutas om: <br></br>
+                {
+                   (date.getFullYear()) +  "-" +
+                   (date.getMonth() + 1 < 10 ? "0" +(date.getMonth() + 1) : date.getMonth() + 1) +"-" + 
+                   (date.getDate() < 10 ? "0" + date.getDate() : date.getDate()) + " " + " "+
+                   (date.getHours() < 10 ? "0" + date.getHours() : date.getHours()) + ":" +
+                   (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()) + ":" +
+                   (date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds())
+                    
+                    }
+                </Typography>
             </Container>
           </Stack>
           <Stack direction="column" gap={2} sx={{ mt: 2 }}>
