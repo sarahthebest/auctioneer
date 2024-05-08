e9c14850862b56fedee802ff9486c6189f8627b9
sarah <sarah.emmoth@proton.me>
2024-05-08 11:27:23 +0200

    tar bort searchresults import från search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import SearchResults from "./SearchResults";
 import { useNavigate } from "react-router-dom";
 
 const URL = "https://auctioneer.up.railway.app/auction/p7u";
